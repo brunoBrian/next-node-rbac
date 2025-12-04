a65c583cba98aa633faf88560af9c0632266e723
brunoBrian <bruno.brian@vemprojuca.com>
2025-12-04 14:42:14 -0300

    adding swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,56 +1,48 @@
 /* eslint-disable prettier/prettier */
-import fastifyCors from '@fastify/cors'
-import fastifySwagger from '@fastify/swagger'
-import fastifySwaggerUI from '@fastify/swagger-ui'
-import fastify from 'fastify'
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastify from "fastify";
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
-} from 'fastify-type-provider-zod'
+} from "fastify-type-provider-zod";
 
-import { createAccount } from './routes/auth/create-account'
+import { createAccount } from "./routes/auth/create-account";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'SaaS',
-      description: 'Specifications for the API of the SaaS platform.',
-      version: '1.0.0',
+      title: "Next.js SaaS",
+      description: "Full-stack SaaS with multi-tenant & RBAC.",
+      version: "1.0.0",
     },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
+    servers: [],
   },
   transform: jsonSchemaTransform,
-})
+});
 
 app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
-})
+  routePrefix: "/docs",
+});
 
-app.register(fastifyCors)
+app.register(fastifyCors);
 
-app.register(createAccount)
+app.register(createAccount);
 
 app.setErrorHandler((error, _, reply) => {
-  console.error(error)
+  console.error(error);
 
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
+  return reply.status(500).send({ message: "Internal server error." });
+});
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!')
-})
+  console.log("HTTP server running!");
+});
