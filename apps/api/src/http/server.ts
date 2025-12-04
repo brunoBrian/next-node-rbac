a6e3fb644c73ec64c14d7c20db2b5e356c376286
brunoBrian <bruno.brian@vemprojuca.com>
2025-12-04 17:36:17 -0300

    creating autenticate with password route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import fastify from "fastify";
@@ -10,6 +11,7 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
+import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
 import { createAccount } from "./routes/auth/create-account";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -33,9 +35,14 @@ app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
 });
 
+app.register(fastifyJwt, {
+  secret: "my-jwt-secret",
+});
+
 app.register(fastifyCors);
 
 app.register(createAccount);
+app.register(authenticateWithPassword);
 
 app.setErrorHandler((error, _, reply) => {
   console.error(error);
