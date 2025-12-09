5fbc52815109b29d76c766eabe7f70f8fb5f341a
brunoBrian <bruno.brian@vemprojuca.com>
2025-12-09 20:07:48 -0300

    add get pending invite service

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,7 +23,9 @@ import { acceptInvite, acceptInvite } from "./routes/invites/accept-invite";
 import { createInvite } from "./routes/invites/create-invite";
 import { getInvite } from "./routes/invites/get-invite";
 import { getInvites } from "./routes/invites/get-invites";
+import { getPendingInvites } from "./routes/invites/get-pending-invites";
 import { rejectInvite } from "./routes/invites/reject-invite";
+import { revokeInvite, revokeInvite } from "./routes/invites/revoke-invite";
 import { getMembers, getMembers } from "./routes/members/get-members";
 import { removeMember } from "./routes/members/remove-member";
 import { updateMember, updateMember } from "./routes/members/update-member";
@@ -39,7 +41,6 @@ import { deleteProject } from "./routes/projects/delete-project";
 import { getProject } from "./routes/projects/get-project";
 import { getProjects } from "./routes/projects/get-projects";
 import { updateProject } from "./routes/projects/update-project";
-import { revokeInvite } from "./routes/invites/revoke-invite";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -109,6 +110,7 @@ app.register(getInvites);
 app.register(acceptInvite);
 app.register(rejectInvite);
 app.register(revokeInvite);
+app.register(getPendingInvites);
 
 app.setErrorHandler((error, _, reply) => {
   console.error(error);
