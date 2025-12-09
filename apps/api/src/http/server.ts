f4ed27a6e154616c81f3742be7216a40cc1be577
brunoBrian <bruno.brian@vemprojuca.com>
2025-12-09 19:40:49 -0300

    add transfer org service

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,8 +23,9 @@ import { createOrganization } from "./routes/orgs/create-organization";
 import { getMembership } from "./routes/orgs/get-membership";
 import { getOrganization } from "./routes/orgs/get-organization";
 import { getOrganizations } from "./routes/orgs/get-organizations";
-import { updateOrganization } from "./routes/orgs/update-organization";
 import { shutdownOrganization } from "./routes/orgs/shutdown-organization";
+import { updateOrganization } from "./routes/orgs/update-organization";
+import { transferOrganization } from "./routes/orgs/transfer-organization";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -76,6 +77,7 @@ app.register(getOrganization);
 app.register(getOrganizations);
 app.register(updateOrganization);
 app.register(shutdownOrganization);
+app.register(transferOrganization);
 
 app.setErrorHandler((error, _, reply) => {
   console.error(error);
