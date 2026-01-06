3a67e63c83d4cc586d186e7be39b540d9afeaa0b
brunoBrian <bruno.brian@vemprojuca.com>
2026-01-06 05:11:46 -0300

    add accept invite page and action

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -1,31 +1,41 @@
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers"
+import { NextRequest, NextResponse } from "next/server"
 
-import { signInWithGithub } from "@/http/sign-in-with-github";
+import { acceptInvite } from "@/http/accept-invite"
+import { signInWithGithub } from "@/http/sign-in-with-github"
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
+  const searchParams = request.nextUrl.searchParams
 
-  const code = searchParams.get("code");
+  const code = searchParams.get("code")
 
   if (!code) {
     return NextResponse.json(
       { message: "Github OAuth  code was not found." },
-      { status: 400 },
-    );
+      { status: 400 }
+    )
   }
 
-  const { token } = await signInWithGithub({ code });
+  const { token } = await signInWithGithub({ code })
 
-  (await cookies()).set("token", token, {
+  ;(await cookies()).set("token", token, {
     path: "/",
     maxAge: 60 * 60 * 24 * 7, // 7days
-  });
+  })
 
-  const redirectUrl = request.nextUrl.clone();
+  const inviteId = (await cookies()).get("inviteId")?.value
 
-  redirectUrl.pathname = "/";
-  redirectUrl.search = "";
+  if (inviteId) {
+    try {
+      await acceptInvite(inviteId)
+      ;(await cookies()).delete("inviteId")
+    } catch {}
+  }
 
-  return NextResponse.redirect(redirectUrl);
+  const redirectUrl = request.nextUrl.clone()
+
+  redirectUrl.pathname = "/"
+  redirectUrl.search = ""
+
+  return NextResponse.redirect(redirectUrl)
 }
