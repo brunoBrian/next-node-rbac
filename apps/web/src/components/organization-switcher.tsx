812a0af21f97d65248106e1f664532ae5e630bfa
brunoBrian <bruno.brian@vemprojuca.com>
2026-01-06 05:26:28 -0300

    add pending invites popover

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,11 +1,10 @@
-import { ChevronsUpDown, PlusCircle } from "lucide-react";
-import { cookies } from "next/headers";
-import Link from "next/link";
+import { ChevronsUpDown, PlusCircle } from "lucide-react"
+import Link from "next/link"
 
-import { getCurrentOrg } from "@/auth/auth";
-import { getOrganizations } from "@/http/get-organizations";
+import { getCurrentOrg } from "@/auth/auth"
+import { getOrganizations } from "@/http/get-organizations"
 
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,42 +13,42 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
+} from "./ui/dropdown-menu"
 
 export async function OrganizationSwitcher() {
-  const currentOrg = await getCurrentOrg();
+  const currentOrg = await getCurrentOrg()
 
-  const { organizations } = await getOrganizations();
+  const { organizations } = await getOrganizations()
 
   const currentOrganization = organizations.find(
-    (org) => org.slug === currentOrg,
-  );
+    (org) => org.slug === currentOrg
+  )
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+      <DropdownMenuTrigger className='flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary'>
         {currentOrganization ? (
           <>
-            <Avatar className="size-4">
+            <Avatar className='size-4'>
               {currentOrganization.avatarUrl && (
                 <AvatarImage src={currentOrganization.avatarUrl} />
               )}
               <AvatarFallback />
             </Avatar>
-            <span className="truncate text-left">
+            <span className='truncate text-left'>
               {currentOrganization.name}
             </span>
           </>
         ) : (
-          <span className="text-muted-foreground">Select organization</span>
+          <span className='text-muted-foreground'>Select organization</span>
         )}
-        <ChevronsUpDown className="ml-auto size-4 text-muted-foreground" />
+        <ChevronsUpDown className='ml-auto size-4 text-muted-foreground' />
       </DropdownMenuTrigger>
       <DropdownMenuContent
-        align="end"
+        align='end'
         alignOffset={-16}
         sideOffset={12}
-        className="w-[200px]"
+        className='w-[200px]'
       >
         <DropdownMenuGroup>
           <DropdownMenuLabel>Organizations</DropdownMenuLabel>
@@ -57,26 +56,26 @@ export async function OrganizationSwitcher() {
             return (
               <DropdownMenuItem key={organization.id} asChild>
                 <Link href={`/org/${organization.slug}`}>
-                  <Avatar className="mr-2 size-4">
+                  <Avatar className='mr-2 size-4'>
                     {organization.avatarUrl && (
                       <AvatarImage src={organization.avatarUrl} />
                     )}
                     <AvatarFallback />
                   </Avatar>
-                  <span className="line-clamp-1">{organization.name}</span>
+                  <span className='line-clamp-1'>{organization.name}</span>
                 </Link>
               </DropdownMenuItem>
-            );
+            )
           })}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href="/create-organization">
-            <PlusCircle className="mr-2 size-4" />
+          <Link href='/create-organization'>
+            <PlusCircle className='mr-2 size-4' />
             Create new
           </Link>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  );
+  )
 }
