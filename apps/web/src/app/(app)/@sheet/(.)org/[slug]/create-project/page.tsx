bf0679d1ae511647daac5c099708d651f7afc947
brunoBrian <bruno.brian@vemprojuca.com>
2026-01-05 21:00:57 -0300

    add interception to cfreate project page

---
+++ apps/web/src/app/(app)/@sheet/(.)org/[slug]/create-project/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { ProjectForm } from "@/app/(app)/org/[slug]/create-project/project-form";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+export default function CreateProject() {
+  const router = useRouter();
+  return (
+    <Sheet
+      defaultOpen
+      onOpenChange={() => {
+        router.back();
+      }}
+    >
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Create organization</SheetTitle>
+        </SheetHeader>
+        <div className="px-4 py-4">
+          <ProjectForm />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
