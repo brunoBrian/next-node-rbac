eadbfb0c8782260133f03d855f1e63319f4f3daa
brunoBrian <bruno.brian@vemprojuca.com>
2026-01-06 03:53:43 -0300

    create members list page

--- apps/web/src/app/(app)/org/[slug]/settings/shutdown-organization-button.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/shutdown-organization-button.tsx
@@ -1,27 +1,27 @@
-import { XCircle } from "lucide-react";
-import { redirect } from "next/navigation";
+import { XCircle } from "lucide-react"
+import { redirect } from "next/navigation"
 
-import { getCurrentOrg } from "@/auth/auth";
-import { Button } from "@/components/ui/button";
-import { shutdownOrganization } from "@/http/shutdown-organization";
+import { getCurrentOrg } from "@/auth/auth"
+import { Button } from "@/components/ui/button"
+import { shutdownOrganization } from "@/http/shutdown-organization"
 
 export function ShutdownOrganizationButton() {
   async function shutdownOrganizationAction() {
-    "use server";
+    "use server"
 
-    const currentOrg = await getCurrentOrg();
+    const currentOrg = await getCurrentOrg()
 
-    await shutdownOrganization({ org: currentOrg! });
+    await shutdownOrganization({ org: currentOrg! })
 
-    redirect("/");
+    redirect("/")
   }
 
   return (
     <form action={shutdownOrganizationAction}>
-      <Button type="submit" variant="destructive" className="w-56">
-        <XCircle className="mr-2 size-4" />
+      <Button type='submit' variant='destructive' className='w-56'>
+        <XCircle className='mr-2 size-4' />
         Shutdown organization
       </Button>
     </form>
-  );
+  )
 }
--- apps/web/src/http/get-membership.ts
+++ apps/web/src/http/get-membership.ts
@@ -1,20 +1,20 @@
-import { Role } from "@saas/auth";
+import { Role } from "@saas/auth"
 
-import { api } from "./api-client";
+import { api } from "./api-client"
 
 interface GetMembershipResponse {
   membership: {
-    id: string;
-    role: Role;
-    organizationId: string;
-    userId: string;
-  };
+    id: string
+    role: Role
+    organizationId: string
+    userId: string
+  }
 }
 
 export async function getMembership(org: string) {
   const result = await api
     .get(`organization/${org}/membership`)
-    .json<GetMembershipResponse>();
+    .json<GetMembershipResponse>()
 
-  return result;
+  return result
 }
